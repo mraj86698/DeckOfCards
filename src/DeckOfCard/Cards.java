c2bd116051c2b2a4661a4b1db34c0bdbea054add
MohanRaj <mraj86698@gmail.com>
2022-07-30 20:02:01 +0530

    Added to Welcome Message

---
+++ src/DeckOfCard/Cards.java
@@ -0,0 +1,13 @@
+package DeckOfCard;
+
+public class Cards {
+	/**
+	 * Simply created a Welcome Message Method without any return type.
+	 */
+	 public void welcome() {
+
+	        System.out.println("Welcome to the gaming world of Deck of Cards");
+
+	    }
+
+}
---
+++ src/DeckOfCard/DeckOfCards.java
@@ -0,0 +1,12 @@
+package DeckOfCard;
+
+public class DeckOfCards {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cards cards = new Cards();
+		cards.welcome();
+
+	}
+
+}
